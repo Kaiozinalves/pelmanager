2981032e75d082e54234abc9e08be3418252886b
Kaio Victor Alves da Rocha <kaio@MacBook-Air-de-Kaio.local>
2026-09-13 13:31:22 -0300

    feat: implementa agendamento de rodadas e entidade de endereço

---
+++ src/main/java/com/pelmanager/repository/RodadaRepository.java
@@ -0,0 +1,7 @@
+package com.pelmanager.repository;
+
+import com.pelmanager.entity.Rodada;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RodadaRepository extends JpaRepository<Rodada, Long> {
+}
